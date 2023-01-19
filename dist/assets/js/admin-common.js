f90617001e76443848d2c7405ebe875faa22c999
red <red@wordncode.com>
2023-01-19 09:44:05 +0900

    [red] 230119 관리자 레이아웃 작업중

---
+++ dist/assets/js/admin-common.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    menu();
+});
+
+
+
+function menu() {
+    const list = document.querySelectorAll('.list');
+    function activeLink() {
+        list.forEach((item) =>
+        item.classList.remove('active'));
+        this.classList.add('active');
+    }
+
+    list.forEach((item) =>
+        item.addEventListener('click', activeLink));
+}
